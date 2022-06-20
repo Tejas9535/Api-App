82a629b68ff506fc0ea2e755afb080845ddef94b
tejas <tmodi95@gmail.com>
2022-06-20 06:44:50 +0530

    Task-2 Added peer-deeps & removed unused files

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -6,7 +6,6 @@ import App from '../App';
 describe('first-test', () => {
   test('renders learn react link', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+    expect(screen.getByText(/learn react/i)).toBeInTheDocument();
   });
 });
