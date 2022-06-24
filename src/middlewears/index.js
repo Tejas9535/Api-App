3b014b0f1b800229d797f1e0f2fe9c80c322feaa
tejas <tmodi95@gmail.com>
2022-06-24 08:38:21 +0530

    Task-5-resolved pipeline failing issy

--- src/middlewears/index.js
+++ src/middlewears/index.js
@@ -1,5 +1,4 @@
 import apiSaga from "./apiSaga";
 import { apiType, API_INVOCATION } from "./constants";
-import rootSaga from "./rootSaga";
 
-export default { apiSaga, apiType, API_INVOCATION, rootSaga };
+export default { apiSaga, apiType, API_INVOCATION };
