a1a6a6ae5a6955bfff5e3441869627732d9aed0e
tejas <tmodi95@gmail.com>
2022-06-30 07:29:42 +0530

    Task-6.1- configured tailwindcss for the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "./common/Navbar/Navbar";
 import AnimeNews from "./components/animeNews";
 import Memes from "./components/memesPage";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Memes />} />
+      <Route path="/" element={<Navbar />} />
+      <Route path="/memes" element={<Memes />} />
       <Route path="/anime" element={<AnimeNews />} />
     </Routes>
   );
---
+++ src/common/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="container">
+      <div className="flex">
+        <div className="logo h-2">Navbar</div>
+        <div className="flex list-none mx-auto justify-end">
+          <li>dummy 1</li>
+          <li>dummy 2</li>
+          <li>dummy 3</li>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
