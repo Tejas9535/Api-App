7acefd894e89e379506155a459471a46b887111b
tejas <tmodi95@gmail.com>
2022-07-02 20:40:06 +0530

    Task-6.2-added dynamic components and themes

---
+++ src/common/index.js
@@ -0,0 +1,6 @@
+import Navbar from "./Navbar/Navbar";
+import Spinner from "./Spinner/Spinner";
+import Card from "./card/Card";
+import Heading from "./Heading/Heading";
+
+export { Navbar, Spinner, Card, Heading };
