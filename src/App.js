122840e46e1e2ef34cf5051884faabf5e6fed317
Tejas <tmodi95@gmail.com>
2022-07-10 00:39:28 +0530

    dummy-commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./common";
 import AnimeNews from "./components/animeNews";
-import Memes from "./components/memesPage";
+// import Memes from "./components/memesPage";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/memes" element={<Memes />} />
+        {/* <Route path="/memes" element={<Memes />} /> */}
         <Route path="/anime" element={<AnimeNews />} />
       </Routes>
     </>
