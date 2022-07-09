a1e9e564cee0b412bddaa3dee2f5cd60647250d2
Tejas <tmodi95@gmail.com>
2022-07-10 02:08:39 +0530

    added test cases for animeNews

--- src/components/animeNews/test/AnimeNews.test.jsx
+++ src/components/animeNews/test/AnimeNews.test.jsx
@@ -1,18 +1,13 @@
-// import React from "react";
-// import { render, screen } from "@testing-library/react";
-// import AnimeNews from "../AnimeNews";
+import React from "react";
+import { render, screen } from "../../../testSetup";
+import AnimeNews from "../AnimeNews";
 
-// describe("AnimeNews", () => {
-//   test("should test heading", async () => {
-//     render(<AnimeNews />);
-//     expect(await screen.findByRole("heading", { level: 1 }));
-//   });
-// });
-
-function sum(a, b) {
-  return a + b;
-}
-
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("AnimeNews", () => {
+  test("should test heading", async () => {
+    const props = {
+      fetchAnimeNews: jest.fn(),
+    };
+    render(<AnimeNews {...props} />);
+    expect(await screen.findByRole("heading", { level: 1 }));
+  });
 });
