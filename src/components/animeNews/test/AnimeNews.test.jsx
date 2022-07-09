cdd1f06f7cb165e95059dce329c739ea84df9a32
Tejas <tmodi95@gmail.com>
2022-07-10 02:23:25 +0530

    added test cases for animeNews

--- src/components/animeNews/test/AnimeNews.test.jsx
+++ src/components/animeNews/test/AnimeNews.test.jsx
@@ -10,4 +10,17 @@ describe("AnimeNews", () => {
     render(<AnimeNews {...props} />);
     expect(await screen.findByRole("heading", { level: 1 }));
   });
+  test("should test heading", async () => {
+    const props = {
+      fetchAnimeNews: jest.fn(),
+      animeNews: [
+        {
+          name: "tejas",
+        },
+      ],
+    };
+    render(<AnimeNews {...props} />);
+    expect(await screen.findByRole("heading", { level: 1 }));
+    expect(await screen.findByText("tejas"));
+  });
 });
